0970806f18b472a7c93deb76cec80af76657762f
Cory <cjbenicak@gmail.com>
2022-08-23 23:52:24 -0400

    intialized react app, removed bloatware, created nav component

---
+++ cory/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Nav from './components/Nav'
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Nav />
+      </header>
+    </div>
+  )
+}
+
+export default App
