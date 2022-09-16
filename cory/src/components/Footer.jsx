f57c08f1c474f489bd6eb8d4d5c3bdc67d495aad
Cory <cjbenicak@gmail.com>
2022-09-16 12:31:15 -0400

    updated footer

--- cory/src/components/Footer.jsx
+++ cory/src/components/Footer.jsx
@@ -1,9 +1,10 @@
 const Footer = () => {
   return(
     <div className="footer">
-      <h4>Connect with me!</h4>
-      <a href="https://github.com/cbenks">GitHub</a>
-      <a href="https://www.linkedin.com/in/corybenicak/">LinkedIn</a>
+      <h4 className="foot 1">Connect with me!</h4>
+      <div className="foot 2"><a href="https://github.com/cbenks">GitHub</a></div>
+      <div className="foot 3"><a href="https://www.linkedin.com/in/corybenicak/">LinkedIn</a></div>
+      <div className="foot 4">Benicak.Cory@gmail.com</div>
     </div>
   )
 }
