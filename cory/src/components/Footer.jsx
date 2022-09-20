115a4adbc42025a5066d8893567dd7f702842fc7
Cory <cjbenicak@gmail.com>
2022-09-20 01:02:50 -0400

    styling on footer

--- cory/src/components/Footer.jsx
+++ cory/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return(
     <div className="footer">
-      <h4 className="foot-a">Connect with me!</h4>
+      <div className="foot-a"><h4 className='connect'>Connect with me!</h4></div>
       <div className="foot-b"><a href="https://github.com/cbenks">GitHub</a></div>
       <div className="foot-c"><a href="https://www.linkedin.com/in/corybenicak/">LinkedIn</a></div>
       <div className="foot-d">Benicak.Cory@gmail.com</div>
