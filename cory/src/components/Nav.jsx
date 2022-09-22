21489014f183bd08e40d0cc3102b55961f9b02e7
Cory <cjbenicak@gmail.com>
2022-09-22 13:18:26 -0400

    styling header

--- cory/src/components/Nav.jsx
+++ cory/src/components/Nav.jsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom'
 
 const Nav = () => {
-  return <div>
-    <div>
-      <img className="nav-logo" alt="Cory B logo" src="https://freelogocreator.com/user_design/logos/2022/08/23/64393-medium.png" />
+  return (
+    <div className="nav">
+      <div>
+        <img className="nav-logo" alt="Cory B logo" src="https://freelogocreator.com/user_design/logos/2022/08/23/64393-medium.png" />
+      </div>
+      <nav>
+        <Link to='/'>About me</Link>
+        <Link to='/projects'>Projects</Link>
+        <Link to='/resume'>Resume</Link>
+      </nav>
     </div>
-    <nav>
-      <Link to='/'>About me</Link>
-      <Link to='/projects'>Projects</Link>
-      <Link to='/resume'>Resume</Link>
-    </nav>
-  </div>
+  )
 }
 export default Nav
